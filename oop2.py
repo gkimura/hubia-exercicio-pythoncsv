3b492ba9837b133cc29c0a048f5c852fc7e74caa
gabriela <gykimura10@gmail.com>
2019-10-17 17:08:22 -0300

    Update

--- oop2.py
+++ oop2.py
@@ -1,62 +1,50 @@
 import numpy as np
 import sys
 
-def fastread (arquivoPath):
+def read_file (arquivoPath):
 	
 	arquivo = None
-
 	dados = list()
 
 	try:
-		
 		arquivo = open(arquivoPath, "r")
-		
-		dados = arquivo.readlines() # cast list()
-		
+		dados = arquivo.readlines()
 		
 	except IOError as e:
 		print(e.args)
-
 		return None
 
-	
 	arquivo.close()
 
 	return list(  map(lambda linha: linha.split(";"),  dados ) )
 
-
-# lambda = *void
-
-
-def verificarVazio(x):
+def isEmpty(x):
 	return x == "Sem Informações" or x == ""
 
-
-def limpa(dataFrame):
+def nEmpty(dataFrame):
 	aux = list()
 	for linha in dataFrame:
-		aux.append( list(filter( verificarVazio ,  linha ) ) )
+		aux.append( list(filter( isEmpty ,  linha ) ) )
 		
-	return list( map(len, aux) )
+	return list(map(len,aux))
 
-
-def meanlimpa(dataFrame):
-	return np.mean (limpa(dataFrame))
+def mean_empty(dataFrame):
+	return np.mean (nEmpty(dataFrame))
 
 
 def transpose(dataFrame):
 	return np.matrix(dataFrame).transpose().tolist()
 
-def inicio_fim_tax(dados):
+def tax_init_end(dados):
 	return dados[0].index("Filo"), dados[0].index("Especie")
 
-def nivel_tax(dados):
+def tax_level(dados):
 	aux = {}
-	init,fim = inicio_fim_tax(dados)	
+	init,fim = tax_init_end(dados)	
 	print(init,fim)
 	for i, line in enumerate(dados):
 		for ind in range(init,fim+1):
-			if verificarVazio(line[ind]):
+			if isEmpty(line[ind]):
 				aux[i]= dados[0][ind-1]
 				break 
 		aux[i] = dados[0][ind]
@@ -75,14 +63,14 @@ def filtro_estados(dados, filtro, valor):
 if __name__ == "__main__":
 	
 	filename = "portalbio_export_16-10-2019-14-39-54.csv"
-	dados = fastread(filename)
-	transposta = transpose(dados)
-	#print(meanlimpa(transposta))
-	#print(nivel_tax(dados))
+	data = read_file(filename)
+	transpost = transpose(data)
+	#print(mean_empty(transpost))
+	print(tax_level(data))
 	
 	#filtro  = input("filtro: (estado/especie/ameaca) ")
 	#valor = input("valor: ")
-	#print(filtro_estados(dados,filtro,valor))
+	#print(filtro_estados(data,filtro,valor))
 
 	
 	
